a64fb7f7d40e0f11bb57994c075fc8ad5c8f75f3
juanfer2 <jfvilladiego3@gmail.com>
2020-11-29 14:15:42 -0500

    Bands

--- backend/models/sockets.js
+++ backend/models/sockets.js
@@ -1,7 +1,8 @@
+const BandList = require('./band-list')
 class Sockets {
   constructor(io) {
     this.io = io;
-
+    this.bandList = new BandList()
     this.socketEvents();
   }
 
@@ -9,13 +10,26 @@ class Sockets {
     // On connection
     this.io.on("connection", (socket) => {
       // Escuchar evento: mensaje-to-server
+      console.log('Client connect')
+      //
+      socket.emit("current-bands", this.bandList.getBands())
+
+      socket.on('add-band', (name) => {
+        this.bandList.addBand(name)
+        const bands = this.bandList.getBands()
+        this.io.emit("current-bands", bands)
+      })
+      /** 
       socket.on("mensaje-to-server", (data) => {
         console.log(data);
 
         this.io.emit("mensaje-from-server", data);
       });
+      */
     });
   }
+
+
 }
 
 module.exports = Sockets;
